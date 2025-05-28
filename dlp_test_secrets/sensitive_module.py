46aad896df0f72aff5afb4a5a86bd7ef7b0ed116
Adriano Maciel <adriano@ambrait.com>
2025-05-28 11:56:08 -0300

    DLP Test: Adicionando arquivo com segredos para teste de vazamento

---
+++ dlp_test_secrets/sensitive_module.py
@@ -0,0 +1,5 @@
+# sensitive_module.py
+API_KEY_PYTHON = "PYTHON_KEY_EXAMPLE_zyxwvu67890"
+DATABASE_PASSWORD_PYTHON = "myPythonDbPassw0rd@"
+print("This Python module contains sensitive data for DLP testing.")
+# Another sensitive token: USER_PYTHON_TOKEN_MLKJIH54321
---
+++ dlp_test_secrets/sensitive_script.php
@@ -0,0 +1,7 @@
+<?php
+// sensitive_script.php
+$api_key_php = "PHP_KEY_EXAMPLE_abcdef12345";
+$db_password_php = "myPhpDbPassw0rd!";
+echo "This PHP script contains sensitive data for DLP testing.";
+// Another sensitive token: USER_PHP_TOKEN_GHIJKL09876
+?>
